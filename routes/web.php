d73c8eccf588b881b1a9b2d5e22e445b0a5efb6b
Vladislav Lukin <vladislav.lukin@brainysoft.ru>
2025-10-19 09:50:06 +0300

    feat(games): remove web routes

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,7 @@
 <?php
 
-use App\Http\Controllers\GamesController;
-use App\Http\Controllers\GameStatsController;
-use App\Http\Controllers\LeaguesController;
-use App\Http\Controllers\LeagueTournamentsController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('leagues', LeaguesController::class)->only([
-    'index'
-]);
-
-Route::resource('leagues.tournaments', LeagueTournamentsController::class)->only([
-    'index', 'show'
-]);
-
-Route::resource('games', GamesController::class)->only([
-    'index', 'show'
-]);
-
-Route::resource('games.stats', GameStatsController::class)->only([
-    'index'
-]);
+Route::get('/', function () {
+    return view('welcome');
+});
