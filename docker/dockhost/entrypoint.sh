f772d9a5f920604187d1665e18863f1115672f8f
Vladislav Lukin <vladislav.lukin@brainysoft.ru>
2025-10-21 11:13:03 +0300

    dockhost hotfix

--- docker/dockhost/entrypoint.sh
+++ docker/dockhost/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Создаем .env файл из переменных окружения
-cat > /var/www/html/.env << EOF
+cat > /usr/share/nginx/html/.env << EOF
 IMP_API_URL="${IMP_API_URL}"
 IMP_CALCULATOR_URL="${IMP_CALCULATOR_URL}"
 EOF
 
-# Запускаем Apache
-exec nginx -g daemon off
+# Запускаем nginx
+exec nginx -g "daemon off;"
